080be0fba217bf54f988d6c539d1b833de6b851d
Abigia Debebe <Abitibebu123@gmail.com>
2023-05-24 21:09:52 +0300

    No breach still not working

--- Utilities/Peek.js
+++ Utilities/Peek.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
         var url = `https://www.dictionaryapi.com/api/v3/references/sd3/json/${smn}?key=db07b340-d7ad-41cf-b150-9efdf351ec75`;
         const response = await fetch(url);
         const data = await response.json();
-        if (Array.isArray(data) && data.length > 0) {
+        if (data[0].meta != null) {
           res.push(`'${smn}'`);
           foundMatch = true;
         }
@@ -74,20 +74,16 @@ $(document).ready(function () {
     var url = `https://api.pwnedpasswords.com/range/${prefix}`;
     $.get(url, async function (string) {
       const hashArray = string.split("\n");
+      var numBreaches = null;
       for (let i = 0; i < hashArray.length; i++) {
         const hash = hashArray[i].split(":");
         if (hash[0] === suffix.toUpperCase()) {
-          const numBreaches = parseInt(hash[1]);
+          numBreaches = parseInt(hash[1]);
 
-          // var dictionary = "null";
-          // try {
-          //   dictionary = await testDictionary(newVal);
-          // } catch {
-          //   dictionary = "hello";
-          // }
+          const dictionary = await testDictionary(newVal);
 
           const resultText1 = `This password has been leaked <h6 class = "part">${numBreaches}</h6> times.`;
-          const resultText2 = `This password contains the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo"> common dictionary words: </a> <h6 class = "part">${"dictionary"}</h6>`;
+          const resultText2 = `This password contains the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo"> common dictionary words: </a> <h6 class = "part">${dictionary}</h6>`;
           const resultText3 = `Assuming a set length, this password would take up to <h6 class = "part">brute</h6> to hack using <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">brute-force.</a
                 >`;
           const resultText4 = `However, the password would take <h6 class = "part">timegiven</h6> times as long as the most hacked password <br> (which could take mere seconds) to crack using a <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">standard dictionary attack.</a
@@ -124,6 +120,60 @@ $(document).ready(function () {
           }, 7500);
           break;
         }
+      }
+
+      if (numBreaches == null) {
+        numBreaches = 0;
+
+        const resultText1 = `This password has been leaked <h6 class = "part">${numBreaches}</h6> times.`;
+        const resultText2 = `That is approximately <h6 class = "part">${numBreaches}%</h6> less than the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo">most leaked password.</a
+          >`;
+        const resultText3 = `Assuming set length, it'd take up to <h6 class = "part">${numBreaches}</h6> to hack via <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">brute-force.</a
+          >`;
+        const resultText4 = `This password would probably not be cracked using a <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">standard dictionary attack.</a
+          >`;
+        const resultText5 = `...Congratulations`;
+        $("<div>")
+          .attr("id", "result1")
+          .addClass("results")
+          .insertAfter(".navbar")
+          .html(resultText1);
+        // changeColors(answer);
+        setTimeout(function () {
+          $("<div>")
+            .attr("id", "result2")
+            .addClass("results")
+            .insertAfter("#result1")
+            .html(resultText2);
+          // changeColors(answer);
+        }, 2500);
+        setTimeout(function () {
+          $("<div>")
+            .attr("id", "result3")
+            .addClass("results")
+            .insertAfter("#result2")
+            .html(resultText3);
+          // changeColors(answer);
+        }, 5000);
+        setTimeout(function () {
+          $("<div>")
+            .attr("id", "result4")
+            .addClass("results")
+            .insertAfter("#result3")
+            .html(resultText4);
+          // changeColors(answer);
+        }, 7500);
+        setTimeout(function () {
+          $("<div>")
+            .attr("id", "result5")
+            .addClass("results")
+            .insertAfter("#result4")
+            .html(resultText5);
+          // changeColors(answer);
+        }, 10000);
+        setTimeout(function () {
+          $("#Happy").show();
+        }, 11000);
       }
     });
   });
