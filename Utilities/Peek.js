be593aa4db61241461c50837f9973db96962fb79
Abigia Debebe <Abitibebu123@gmail.com>
2023-05-24 22:32:59 +0300

    No display when null issue fixed

--- Utilities/Peek.js
+++ Utilities/Peek.js
@@ -20,7 +20,8 @@ $(document).ready(function () {
     $("#arrow").hide();
   });
 
-  async function testDictionary(word) {
+  async function testDictionary(given) {
+    const word = given.replace(/[^a-zA-Z ]/g, "");
     var res = [];
     var foundMatch = false;
 
@@ -28,7 +29,7 @@ $(document).ready(function () {
       for (let j = i + 3; j < word.length; j++) {
         var smn = word.substring(i, j + 1);
         console.log(smn);
-        var url = `https://www.dictionaryapi.com/api/v3/references/sd3/json/${smn}?key=db07b340-d7ad-41cf-b150-9efdf351ec75`;
+        var url = `https://www.dictionaryapi.com/api/v3/references/sd2/json/${smn}?key=8bd7aebb-af8d-4d2d-837b-b72dd39c3011`;
         const response = await fetch(url);
         const data = await response.json();
         if (data[0].meta != null) {
@@ -54,7 +55,7 @@ $(document).ready(function () {
     return result;
   }
 
-  $("form").on("submit", function (e) {
+  $("form").on("submit", async function (e) {
     e.preventDefault(); // Prevent the default form submission behavior
     $(".navbar").removeClass("navbar1");
     $("#arrow").hide();
@@ -80,44 +81,14 @@ $(document).ready(function () {
         if (hash[0] === suffix.toUpperCase()) {
           numBreaches = parseInt(hash[1]);
 
-          const dictionary = await testDictionary(newVal);
-
           const resultText1 = `This password has been leaked <h6 class = "part">${numBreaches}</h6> times.`;
-          const resultText2 = `This password contains the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo"> common dictionary words: </a> <h6 class = "part">${dictionary}</h6>`;
-          const resultText3 = `Assuming a set length, this password would take up to <h6 class = "part">brute</h6> to hack using <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">brute-force.</a
-                >`;
-          const resultText4 = `However, the password would take <h6 class = "part">timegiven</h6> times as long as the most hacked password <br> (which could take mere seconds) to crack using a <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">standard dictionary attack.</a
-                                      >`;
+
           $("<div>")
             .attr("id", "result1")
             .addClass("results")
             .insertAfter(".navbar")
             .html(resultText1);
           // changeColors(answer);
-          setTimeout(function () {
-            $("<div>")
-              .attr("id", "result2")
-              .addClass("results")
-              .insertAfter("#result1")
-              .html(resultText2);
-            // changeColors(answer);
-          }, 2500);
-          setTimeout(function () {
-            $("<div>")
-              .attr("id", "result3")
-              .addClass("results")
-              .insertAfter("#result2")
-              .html(resultText3);
-            // changeColors(answer);
-          }, 5000);
-          setTimeout(function () {
-            $("<div>")
-              .attr("id", "result4")
-              .addClass("results")
-              .insertAfter("#result3")
-              .html(resultText4);
-            // changeColors(answer);
-          }, 7500);
           break;
         }
       }
@@ -126,55 +97,34 @@ $(document).ready(function () {
         numBreaches = 0;
 
         const resultText1 = `This password has been leaked <h6 class = "part">${numBreaches}</h6> times.`;
-        const resultText2 = `That is approximately <h6 class = "part">${numBreaches}%</h6> less than the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo">most leaked password.</a
-          >`;
-        const resultText3 = `Assuming set length, it'd take up to <h6 class = "part">${numBreaches}</h6> to hack via <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">brute-force.</a
-          >`;
-        const resultText4 = `This password would probably not be cracked using a <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" class="navbar-brand" href="Utilities/About.html#jumpTo">standard dictionary attack.</a
-          >`;
-        const resultText5 = `...Congratulations`;
         $("<div>")
           .attr("id", "result1")
           .addClass("results")
           .insertAfter(".navbar")
           .html(resultText1);
-        // changeColors(answer);
-        setTimeout(function () {
-          $("<div>")
-            .attr("id", "result2")
-            .addClass("results")
-            .insertAfter("#result1")
-            .html(resultText2);
-          // changeColors(answer);
-        }, 2500);
-        setTimeout(function () {
-          $("<div>")
-            .attr("id", "result3")
-            .addClass("results")
-            .insertAfter("#result2")
-            .html(resultText3);
-          // changeColors(answer);
-        }, 5000);
-        setTimeout(function () {
-          $("<div>")
-            .attr("id", "result4")
-            .addClass("results")
-            .insertAfter("#result3")
-            .html(resultText4);
-          // changeColors(answer);
-        }, 7500);
-        setTimeout(function () {
-          $("<div>")
-            .attr("id", "result5")
-            .addClass("results")
-            .insertAfter("#result4")
-            .html(resultText5);
-          // changeColors(answer);
-        }, 10000);
-        setTimeout(function () {
-          $("#Happy").show();
-        }, 11000);
       }
     });
+
+    var dictionary;
+    var resultText2;
+    try {
+      dictionary = await testDictionary(newVal);
+      resultText2 = `This password contains the <a style = "text-decoration: underline; font-size: 16px; color: white" class="navbar-brand" href="Utilities/About.html#jumpTo"> common dictionary words: </a> <h6 class = "part">${dictionary}</h6>`;
+    } catch {
+      resultText2 = `This password doesn't contain any common dictionary words.`;
+    }
+    if (dictionary == null) {
+      resultText2 = `This password doesn't contain any common dictionary words.`;
+    }
+    console.log(dictionary);
+
+    setTimeout(function () {
+      $("<div>")
+        .attr("id", "result2")
+        .addClass("results")
+        .insertAfter("#result1")
+        .html(resultText2);
+      changeColors(answer);
+    }, 500);
   });
 });
