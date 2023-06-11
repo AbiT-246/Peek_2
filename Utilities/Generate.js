9b14c7eca5d7ac09f7775435c492e6a1dac76d3b
Abigia Debebe <Abitibebu123@gmail.com>
2023-06-11 21:34:18 +0300

    Generate Bug Fixed

--- Utilities/Generate.js
+++ Utilities/Generate.js
@@ -1,4 +1,113 @@
 $(document).ready(function () {
+  function shuffle(array) {
+    let currentIndex = array.length,
+      randomIndex;
+
+    while (currentIndex != 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+
+    return array;
+  }
+
+  function hash_u(arr, numb, symbols, lengthy) {
+    var asList,
+      combined,
+      combined_org,
+      newPass,
+      num_asString,
+      numb1,
+      numb2,
+      rand_num1,
+      rand_num2,
+      rand_num3,
+      rand_num4,
+      special;
+    special = [
+      " ",
+      "!",
+      "#",
+      "$",
+      "%",
+      "&",
+      "(",
+      ")",
+      "*",
+      "+",
+      ",",
+      "-",
+      ".",
+      "/",
+      ":",
+      ";",
+      "<",
+      "=",
+      ">",
+      "?",
+      "@",
+      "[",
+      "]",
+      "^",
+      "",
+      "`",
+      "{",
+      "|",
+      "}",
+      "~",
+    ];
+    combined_org = arr[0] + arr[1];
+    combined = [];
+
+    for (var i = 0, _pj_a = combined_org.length; i < _pj_a; i += 3) {
+      if (combined_org.length - i <= 4) {
+        combined.push(combined_org.slice(i));
+      } else {
+        combined.push(combined_org.slice(i, i + 3));
+      }
+    }
+
+    asList = combined;
+    asList = shuffle(asList);
+    rand_num1 = Math.floor(Math.random() * combined.length);
+    rand_num2 = Math.floor(Math.random() * combined.length);
+    num_asString = numb.toString();
+    numb1 = num_asString.slice(0, num_asString.length / 2);
+    numb2 = num_asString.slice(num_asString.length / 2);
+    asList.splice(rand_num1, 0, numb1);
+    asList.splice(rand_num2, 0, numb2);
+
+    newPass = asList.join("");
+
+    if (symbols === 1) {
+      rand_num3 = Math.floor(Math.random() * special.length - 1);
+      rand_num4 = Math.floor(Math.random() * combined.length);
+      asList.splice(rand_num4, 0, special[rand_num3]);
+      newPass = asList.join("");
+
+      while (newPass.length < lengthy) {
+        var rand_num5 = Math.floor(Math.random() * asList.length);
+        var rand_symb = special[Math.floor(Math.random() * special.length)];
+        asList.splice(rand_num5, 0, rand_symb);
+        newPass = asList.join("");
+      }
+    } else {
+      while (newPass.length < lengthy) {
+        var rand_num5 = Math.floor(Math.random() * asList.length);
+        asList.splice(rand_num5, 0, "0");
+        newPass = asList.join("");
+      }
+    }
+
+    newPass = asList.join("");
+    return newPass;
+  }
+
   var symbols = 0;
   $("#symbolChooser").on("click", function (event) {
     $(this).toggleClass("symbol");
